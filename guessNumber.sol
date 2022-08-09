03aa7a048929b8952b99bf60e9048d31e63a0f53
Sobek888 <109724742+Sobek888@users.noreply.github.com>
2022-08-09 09:26:36 +0200

    Update guessNumber.sol

--- guessNumber.sol
+++ guessNumber.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.16;
 
 contract guessTheNumber{
 
-// setting up original number and owner address
+// setting up original nparticipant
     constructor(uint _number, string memory _myName){
-        number = _number;
         owner = msg.sender;
-        participantList.push(participants(0x0000000000000000000000000000000000000000, _myName, 0, 0));
+        number = _number;
+        participantsList.push(participant(0x0000000000000000000000000000000000000000, _myName, 0, 0));
         participantsCounter = 1;
     }
 
@@ -16,42 +16,80 @@ contract guessTheNumber{
     uint private number;
     address public winnersAddr;
     uint public participantsCounter;
-    mapping (address => uint) userCounter;
+    mapping (address => uint) internal userCounter;
     
 // struct for praticipants list
-    struct participants {
+    struct participant {
         address addr;
         string username;
         uint counter;
         uint userId;
     }
 
-    mapping (address => mapping (uint => participants[])) participant;
-    participants[] participantList;
+    participant[] participantsList;
 
 //register participant
     function username(string memory _myName) public {
-        participantsCounter++;
-        uint _idCounter = participantsCounter -1;
-       participantList.push(participants(msg.sender, _myName, 0, _idCounter));
+        uint _index = participantsCounter - 1;
+        participant storage party = participantsList[_index];
+            if (msg.sender != party.addr) { 
+            uint _idCounter = participantsCounter -1;
+            participantsList.push(participant(msg.sender, _myName, 0, _idCounter));
+            participantsCounter++;
+                 } else {
+            require(msg.sender != party.addr, "Already registered");
+            participantsCounter++;
+                }
+    }
+
+// update participant's username
+    function updateUsername(string memory _newName) public {
+        uint _index = participantsCounter - 1;
+        participant storage party = participantsList[_index];
+        if (msg.sender == party.addr) { 
+            party.username = _newName;
+            } else {
+            require(msg.sender == party.addr, "You haven't registered yet");
+            }
     }
 
 //get participant
     function participantId(uint _index) public view returns(address, string memory, uint, uint) {
-        participants storage party = participantList[_index];
+        participant storage party = participantsList[_index];
        return (party.addr, party.username, party.counter, party.userId);
     }
 
 // guess the number
-    function guessNumber(uint _numberGuess) public {
+    function guessNumber(uint _numberGuess) threeAttempts() ifGuessed() ifRegistered() public {
+        userCounter[msg.sender] ++;
+        uint _index = participantsCounter - 1;
+        participant storage _party = participantsList[_index];
+        _party.counter++;
         require(_numberGuess < 10, "One digit number only");
-        require(userCounter[msg.sender] < 4, "Too many attempts");
-            if (_numberGuess == number) {
+
+                if (_numberGuess == number) {
                 (winnersAddr = msg.sender);
-            } else {
-            userCounter[msg.sender] ++;
+                } else {
                 }
-        }
+    }
+
+//if not registered modifier
+    modifier ifRegistered() {
+        uint _index = participantsCounter -1;
+        participant storage party = participantsList[_index];
+        require(msg.sender == party.addr,"Not registered");
+        _;
+    }
+// already guessed modifier
+    modifier ifGuessed() {
+            require(msg.sender != winnersAddr,"YOU already guessed it!");
+            _;
+    }
+
+// 3 attempts modifier
+    modifier threeAttempts() {
+        require(userCounter[msg.sender] < 3, "Too many attemnpts");
+        _;
     }
 
 // set the number
@@ -63,12 +101,10 @@ contract guessTheNumber{
 
 // winner only modifier
     modifier winnerOnly() {
-        require(winnersAddr == msg.sender);
+        require(winnersAddr == msg.sender, "You haven't guessed it!");
         _;
     }
 
 // function guessNumber shoudlnt be called after it being guessed and before setting new number, maybe
-// user should get a timer to set up the new number, if not set to default.
-// if the address has registered, shouldnt create another position and get an error when calling same function,
-// but have possibility to update own usernam
+// user should get a timer to set up the new number and if not set to default.
 }
